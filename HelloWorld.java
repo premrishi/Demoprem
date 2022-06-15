34df31dd7609e109e76de6458677cfee9d7ed1b5
premrishi <premreshee@gmail.com>
2022-06-15 09:30:46 +0530

    Update and rename Java1.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,9 @@
-/**
 
+/** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
-
  */
-
 public class HelloWorld {
-
   public static void main(String[] args) {
-
-    System.out.println("My name is Prem");
-
+    System.out.println("My name is Deeksha");
   }
-
 }
